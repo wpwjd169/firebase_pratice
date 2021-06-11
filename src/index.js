1b3d1846c167c6b3c2b3c9ada73b1e479dbe55d3
wpwjd169 <wpwjd169@naver.com>
2021-06-11 22:19:19 +0900

    파이어베이스 처음으로 로그인 인증까지 성공

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from 'components/app';
+import firebase from "fbase";
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/routes/Auth.js
@@ -0,0 +1,49 @@
+import { authService } from "fbase";
+import React, { useState } from "react";
+
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const onChange = (event) => {
+        const { target: { name, value } } = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } 
+            else {
+                data = await authService.signInWithEmailAndPassword(email, password);
+
+            }
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}></input>
+                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}></input>
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"}></input>
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Contonue with Github</button>
+            </div>
+        </div>
+    )
+}
+
+export default Auth;
